5a253cd49662d629af3d1655f999ec3853da2de8
redwappin <mimili59.didine@hotmail.fr>
2019-06-07 09:27:48 +0200

    rename of update_configuration sql file

--- scripts/update_configuration.bash
+++ scripts/update_configuration.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo -u postgres psql -f ../sql/postgres_conf.sql
+sudo -u postgres psql -f ../sql/update_configuration.sql
 
 #Restart service postgres
 sudo service postgresql restart
